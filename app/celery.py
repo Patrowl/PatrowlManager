5523aae9d3e7714f1f1a212098a0cfc3cd172e2a
Nicolas Mattiocco <nicolas@greenlock.fr>
2019-03-10 22:01:27 +0100

    v1.0.5 prep

--- app/celery.py
+++ app/celery.py
@@ -13,12 +13,12 @@ app = Celery('app', broker=settings.BROKER_URL)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-app.conf.broker_transport_options = {
-    'max_retries': 3,
-    'interval_start': 0,
-    'interval_step': 0.2,
-    'interval_max': 0.2,
-}
+# app.conf.broker_transport_options = {
+#     'max_retries': 3,
+#     'interval_start': 0,
+#     'interval_step': 0.2,
+#     'interval_max': 0.2,
+# }
 
 #app.conf.timezone = os.environ.get('PATROWL_TZ', 'Europe/Paris')
 
