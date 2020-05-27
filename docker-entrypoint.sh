a8db9d8c089ffd0e13d37111bdb224109fe8469a
Nicolas Mattiocco <nicolas@greenlock.fr>
2020-05-27 11:36:14 +0200

    Parameterize ports in Docker-entrypoints

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 export DB_HOST=${DB_HOST:-db}
+export DB_PORT=${DB_PORT:-5432}
 export RABBITMQ_HOST=${RABBITMQ_HOST:-rabbitmq}
+export RABBITMQ_PORT=${RABBITMQ_PORT:-5672}
 
 echo "[+] Wait for DB availability"
-while !</dev/tcp/$DB_HOST/5432; do sleep 1; done
+while !</dev/tcp/$DB_HOST/$DB_PORT; do sleep 1; done
 
 echo "[+] Wait for RabbitMQ availability"
-while !</dev/tcp/$RABBITMQ_HOST/5672; do sleep 1; done
+while !</dev/tcp/$RABBITMQ_HOST/$RABBITMQ_PORT; do sleep 1; done
 
 source env3/bin/activate
 
