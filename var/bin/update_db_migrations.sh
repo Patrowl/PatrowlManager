ac751baceaf1f0eb7decb7748cb257486e3fa5d0
Nicolas Mattiocco <nicolas@greenlock.fr>
2020-07-24 17:51:05 +0200

    Add migrations files in configuration and update db migration script

--- var/bin/update_db_migrations.sh
+++ var/bin/update_db_migrations.sh
@@ -1,3 +1,13 @@
+verlte() {
+    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
+
+# Check version for compatibility, otherwise exit
+verlte `cat VERSION | cut -f1 -d" "` 1.5.4 && echo "Let's go" || { echo "Use current migration files. Exit now" ; exit; }
+
+echo "-- Installed tables:"
+echo "from django.db import connection ; print(connection.introspection.table_names()) " | python manage.py shell
+
 if echo "from django.db import connection ; print('assets' in connection.introspection.table_names()) " | python manage.py shell | grep -q 'True'; then
 
     echo "-- Clean the django_migrations table"
