a26705a275063ae7e929a4cfcd1607f91004bf8b
vtasio <tasiopoulos.vasilis@gmail.com>
2021-02-03 01:36:07 -0800

    fixes

--- engines/utils.py
+++ engines/utils.py
@@ -541,10 +541,6 @@ def _import_findings(findings, scan, engine_name=None, engine_id=None, owner_id=
                     new_tag = _add_asset_tags(asset, 'inactive-domain')
                     asset.categories.add(new_tag)
                     asset.save()
-                if 'Failed to resolve' in finding['title'] and asset.type=="ip":
-                    new_tag = _add_asset_tags(asset, 'inactive-ip')
-                    asset.categories.add(new_tag)
-                    asset.save()
                 if 'Host' in finding['title'] and 'is up' in finding['title'] and asset.type=="domain":
                     new_tag = _add_asset_tags(asset, 'active-domain')
                     asset.categories.add(new_tag)
@@ -619,10 +615,6 @@ def _import_findings(findings, scan, engine_name=None, engine_id=None, owner_id=
                 asset.save()
             if 'Failed to resolve' in rawfinding.title and asset.type=="domain":
                 invalid_tag = _add_asset_tags(asset, 'inactive-domain')
-                asset.categories.remove(invalid_tag)
-                asset.save()
-            if 'Failed to resolve' in rawfinding.title and asset.type=="ip":
-                invalid_tag = _add_asset_tags(asset, 'inactive-ip')
                 asset.categories.remove(invalid_tag)
                 asset.save()
             if 'Host' in rawfinding.title and 'is up' in rawfinding.title and asset.type=="domain":
